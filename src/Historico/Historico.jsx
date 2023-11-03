4dc03c62b8479a4e0537540f7a3a3906a1ffae62
Rafael Reis <rfr9@protonmail.com>
2023-11-03 15:20:42 -0300

    att

--- src/Historico/Historico.jsx
+++ src/Historico/Historico.jsx
@@ -1,6 +1,6 @@
 import { useParams } from 'react-router-dom';
 import React, { useEffect, useState } from 'react';
-import { db, doc, query, getDocs, collection} from '../firebase';
+import { db, doc, query, getDocs, collection } from '../firebase';
 
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
@@ -19,22 +19,22 @@ import LoggedHeader from '../LoggedHeader/LoggedHeader';
 function Historico() {
 
     const { idProf } = useParams();
-    
+
     const [dadosHistorico, setDadosHistorico] = useState();
 
     const buscarHistorico = () => {
         const colecaoPrincipal = collection(db, `profissionais/${idProf}/historico`)
         getDocs(colecaoPrincipal)
             .then(async (querySnapshot) => {
-               const attUsuario = querySnapshot.docs.map((doc) => ({...doc.data(), id:doc.id}));
-               setDadosHistorico(attUsuario);
+                const attUsuario = querySnapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+                setDadosHistorico(attUsuario);
             })
-    } 
+    }
 
     useEffect(() => {
         buscarHistorico();
     }, [])
-    
+
     return (
         <div>
             <LoggedHeader />
@@ -52,92 +52,92 @@ function Historico() {
                         <TableBody>
                             {dadosHistorico?.map((atualizacao, i) => (
                                 <TableRow
-                                    key={i} 
+                                    key={i}
                                     sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
                                 >
                                     <TableCell align="center">{atualizacao.id}</TableCell>
                                     <TableCell>
-                                    <List>
-                                        <ListItem disablePadding>
-                                            <ListItemText>
-                                                E-mail: {atualizacao.att.dadosAntigos.email}
-                                            </ListItemText>                                    
-                                        </ListItem>
+                                        <List>
+                                            <ListItem disablePadding>
+                                                <ListItemText>
+                                                    E-mail: {atualizacao.att.dadosAntigos.email}
+                                                </ListItemText>
+                                            </ListItem>
 
-                                        <ListItem disablePadding>
-                                            <ListItemText>
-                                                Telefone: {atualizacao.att.dadosAntigos.telefone}
-                                            </ListItemText>                                    
-                                        </ListItem>
+                                            <ListItem disablePadding>
+                                                <ListItemText>
+                                                    Telefone: {atualizacao.att.dadosAntigos.telefone}
+                                                </ListItemText>
+                                            </ListItem>
 
-                                        <ListItem disablePadding>
-                                            <ListItemText>
-                                                Endereço: {atualizacao.att.dadosAntigos.endereço}
-                                            </ListItemText>                                    
-                                        </ListItem>
+                                            <ListItem disablePadding>
+                                                <ListItemText>
+                                                    Endereço: {atualizacao.att.dadosAntigos.endereço}
+                                                </ListItemText>
+                                            </ListItem>
 
-                                        <ListItem disablePadding>
-                                            <ListItemText>
-                                                Cargo: {atualizacao.att.dadosAntigos.cargo}
-                                            </ListItemText>                                    
-                                        </ListItem>
+                                            <ListItem disablePadding>
+                                                <ListItemText>
+                                                    Cargo: {atualizacao.att.dadosAntigos.cargo}
+                                                </ListItemText>
+                                            </ListItem>
 
-                                        <ListItem disablePadding>
-                                            <ListItemText>
-                                                Setor: {atualizacao.att.dadosAntigos.setor}
-                                            </ListItemText>                                    
-                                        </ListItem>
+                                            <ListItem disablePadding>
+                                                <ListItemText>
+                                                    Setor: {atualizacao.att.dadosAntigos.setor}
+                                                </ListItemText>
+                                            </ListItem>
 
-                                        <ListItem disablePadding>
-                                            <ListItemText>
-                                                Salário: {atualizacao.att.dadosAntigos.salario}
-                                            </ListItemText>                                    
-                                        </ListItem>
-        
+                                            <ListItem disablePadding>
+                                                <ListItemText>
+                                                    Salário: {atualizacao.att.dadosAntigos.salario}
+                                                </ListItemText>
+                                            </ListItem>
+
                                         </List>
                                     </TableCell>
 
                                     <TableCell>
-                                    <List>
-                                        <ListItem disablePadding>
-                                            <ListItemText>
-                                                E-mail: {atualizacao.att.novosDados.email}
-                                            </ListItemText>                                    
-                                        </ListItem>
+                                        <List>
+                                            <ListItem disablePadding>
+                                                <ListItemText>
+                                                    E-mail: {atualizacao.att.novosDados.email}
+                                                </ListItemText>
+                                            </ListItem>
 
-                                        <ListItem disablePadding>
-                                            <ListItemText>
-                                                Telefone: {atualizacao.att.novosDados.telefone}
-                                            </ListItemText>                                    
-                                        </ListItem>
+                                            <ListItem disablePadding>
+                                                <ListItemText>
+                                                    Telefone: {atualizacao.att.novosDados.telefone}
+                                                </ListItemText>
+                                            </ListItem>
 
-                                        <ListItem disablePadding>
-                                            <ListItemText>
-                                                Endereço: {atualizacao.att.novosDados.endereço}
-                                            </ListItemText>                                    
-                                        </ListItem>
+                                            <ListItem disablePadding>
+                                                <ListItemText>
+                                                    Endereço: {atualizacao.att.novosDados.endereço}
+                                                </ListItemText>
+                                            </ListItem>
 
-                                        <ListItem disablePadding>
-                                            <ListItemText>
-                                                Cargo: {atualizacao.att.novosDados.cargo}
-                                            </ListItemText>                                    
-                                        </ListItem>
+                                            <ListItem disablePadding>
+                                                <ListItemText>
+                                                    Cargo: {atualizacao.att.novosDados.cargo}
+                                                </ListItemText>
+                                            </ListItem>
 
-                                        <ListItem disablePadding>
-                                            <ListItemText>
-                                                Setor: {atualizacao.att.novosDados.setor}
-                                            </ListItemText>                                    
-                                        </ListItem>
+                                            <ListItem disablePadding>
+                                                <ListItemText>
+                                                    Setor: {atualizacao.att.novosDados.setor}
+                                                </ListItemText>
+                                            </ListItem>
 
-                                        <ListItem disablePadding>
-                                            <ListItemText>
-                                                Salário: {atualizacao.att.novosDados.salario}
-                                            </ListItemText>                                    
-                                        </ListItem>
-        
+                                            <ListItem disablePadding>
+                                                <ListItemText>
+                                                    Salário: {atualizacao.att.novosDados.salario}
+                                                </ListItemText>
+                                            </ListItem>
+
                                         </List>
                                     </TableCell>
-                                    
+
                                 </TableRow>
                             ))}
                         </TableBody>
